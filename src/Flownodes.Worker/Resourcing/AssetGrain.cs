ddaf498e1515680a2a8bd841bf0eaaa6b4da6c3b
coderynx <simone.filippi.1999@gmail.com>
2023-03-23 10:25:35 +0100

    Add GrainType to AssetGrain

--- src/Flownodes.Worker/Resourcing/AssetGrain.cs
+++ src/Flownodes.Worker/Resourcing/AssetGrain.cs
@@ -1,8 +1,10 @@
+using Flownodes.Sdk.Entities;
 using Flownodes.Shared.Resourcing.Grains;
 using Flownodes.Worker.Services;
 
 namespace Flownodes.Worker.Resourcing;
 
+[GrainType(FlownodesEntityNames.Asset)]
 internal sealed class AssetGrain : ResourceGrain, IAssetGrain
 {
     public AssetGrain(ILogger<AssetGrain> logger, IEnvironmentService environmentService)
